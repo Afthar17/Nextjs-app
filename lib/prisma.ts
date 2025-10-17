1b28b3c88a146415172c97a4f44c848efc512d53
Afthar17 <aftharafthar5@gmail.com>
2025-10-17 15:01:07 +0530

    dependencies refresh

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,7 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-};
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-});
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
