f367dc93e5f6954a75204ba546bc66967896fc54
Afthar17 <aftharafthar5@gmail.com>
2025-10-17 15:33:47 +0530

    params bug

--- app/trips/[tripId]/itinerary/new/page.tsx
+++ app/trips/[tripId]/itinerary/new/page.tsx
@@ -5,8 +5,8 @@ import NewLocationClient from "@/components/NewLocationClient";
 export default async function NewItinerary({
   params,
 }: {
-  params: { tripId: string };
+  params: Promise<{ tripId: string }>;
 }) {
-  const { tripId } = params;
+  const { tripId } = await params;
   return <NewLocationClient tripId={tripId} />;
 }
